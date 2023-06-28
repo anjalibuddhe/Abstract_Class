517562776c51906294f1ee0dc7aad7a47e22148a
anjali buddhe <anjalibuddhe124@gmail.com>
2023-06-28 08:31:05 +0530

    Add project files.

---
+++ Abstractmethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Abstractmethod.AbstractClass;
+
+namespace Abstractmethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rectanglel area = new Rectanglel(10, 20);
+            area.CalculateArea();
+            Console.WriteLine(area);
+        }
+    }
+}
